eef4499f791f3d043f876d01f7858aa0ce49b625
sanyam-S <49986897+sanyam-S@users.noreply.github.com>
2021-02-24 23:50:03 +0530

    Update background.sh

--- new-scene/background.sh
+++ new-scene/background.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 
-wget https://ol-tsting.s3.amazonaws.com/ol-cli-install.sh
-bash ol-cli-install.sh
+wget https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/latest/linux-macos/openlegacy-cli.zip
+unzip openlegacy-cli.zip
+wget https://ol-tsting.s3.amazonaws.com/install.sh
+mv install.sh /root/ol/bin
+chmod +x /root/ol/bin/install.sh
+cd /root/ol/bin
+./install.sh 
+exec bash -l
