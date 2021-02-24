20ccdd9d01e1d2366ba1cb9d829f98dbfbfc07bd
sanyam-S <49986897+sanyam-S@users.noreply.github.com>
2021-02-24 23:08:36 +0530

    Update background.sh

--- new-scene/background.sh
+++ new-scene/background.sh
@@ -3,8 +3,9 @@
 wget https://ol-public-artifacts.s3.amazonaws.com/openlegacy-cli/latest/linux-macos/openlegacy-cli.zip
 unzip openlegacy-cli.zip
 wget https://ol-tsting.s3.amazonaws.com/install.sh
-mv install.sh ol/bin
-chmod +x ol/bin/install.sh
-cd ol/bin
+mv install.sh /root/ol/bin
+chmod +x /root/ol/bin/install.sh
+cd /root/ol/bin
+apt-get update && apt apt install default-jre -y
 ./install.sh 
 exec bash -l
