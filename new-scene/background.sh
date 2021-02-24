37644557ba7e05d47185b3713e0ae7ecc3ff1e9e
sanyam-S <49986897+sanyam-S@users.noreply.github.com>
2021-02-24 23:25:04 +0530

    Update background.sh

--- new-scene/background.sh
+++ new-scene/background.sh
@@ -6,6 +6,6 @@ wget https://ol-tsting.s3.amazonaws.com/install.sh
 mv install.sh /root/ol/bin
 chmod +x /root/ol/bin/install.sh
 cd /root/ol/bin
-apt-get update && apt apt install default-jre -y
+apt-get update && apt install default-jre -y
 ./install.sh 
 exec bash -l
