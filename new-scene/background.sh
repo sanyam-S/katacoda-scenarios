7bf0a00695b7bb5674216c952b9ac569dbbcbc1b
sanyam-S <49986897+sanyam-S@users.noreply.github.com>
2021-02-26 19:09:17 +0530

    Update background.sh

--- new-scene/background.sh
+++ new-scene/background.sh
@@ -2,4 +2,7 @@
 
 wget https://ol-tsting.s3.amazonaws.com/ol-cli-install.sh
 chmod +x ol-cli-install.sh
-sudo bash ol-cli-install.sh && sudo bash exe.sh && sudo rm ol-cli-install.sh
+sudo bash ol-cli-install.sh
+sudo -i
+#sudo bash exe.sh 
+sudo rm ol-cli-install.sh
